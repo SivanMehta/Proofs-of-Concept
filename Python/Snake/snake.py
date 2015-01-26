a53cd6b402c75e69097e2badeeecdc1e95ee8d97
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-01-25 20:12:10 -0500

    fixed coin placement to accomodate any size board above 100 px

--- Python/Snake/snake.py
+++ Python/Snake/snake.py
@@ -1,6 +1,9 @@
 from Tkinter import *
 import time, copy, sys, random
 
+snakeSize = 5
+boardSize = 1000
+
 class PhysicalObject():
     def __init__(self, x, y, radius, color):
         self.x = x
@@ -17,7 +20,7 @@ class PhysicalObject():
 class Snake():
     def __init__(self):
         self.root = Tk()
-        self.canvas = Canvas(self.root, height = 500, width = 500)
+        self.canvas = Canvas(self.root, height = boardSize, width = boardSize)
         self.canvas.pack()
         self.root.bind("<Key>", self.keyInput)
 
@@ -27,9 +30,11 @@ class Snake():
         self.root.mainloop()
 
     def notInit(self):
-        self.snake = [PhysicalObject(300, 300, 10, "red")]
+        self.snake = [PhysicalObject(300, 500, snakeSize, "red")]
         self.snake[0].color = "green"
-        self.coin = PhysicalObject(200, 200, 10, "blue")
+        self.coin = PhysicalObject(200, 200, snakeSize, "blue")
+        self.eat()
+        self.snake = self.snake[:1]
         self.direction = [0, -1]
 
     def keyInput(self, event):
@@ -46,24 +51,24 @@ class Snake():
         self.animate()
         self.redrawAll()
 
-        self.canvas.after(100, self.timer)
+        self.canvas.after(33, self.timer)
 
     def animate(self):
-        oldSnake = copy.deepcopy(self.snake) # http://en.wikipedia.org/wiki/Metal_Gear_Solid
+        oldSnake = copy.deepcopy(self.snake)
 
         for i in range(1, len(self.snake)):
             self.snake[i].x = oldSnake[i - 1].x
             self.snake[i].y = oldSnake[i - 1].y
 
         head = self.snake[0]
-        head.x += 20 * self.direction[0]
-        head.y += 20 * self.direction[1]
+        head.x += 2 * snakeSize * self.direction[0]
+        head.y += 2 * snakeSize * self.direction[1]
 
         tail = [self.snake[-1].x, self.snake[-1].y]
         hits = False
 
         for segment in self.snake:
-            if head.collides(segment) and segment != head or (head.x <= 0 or head.y <= 0 or head.x >= 500 or head.y >= 500):
+            if head.collides(segment) and segment != head or (head.x <= 0 or head.y <= 0 or head.x >= boardSize or head.y >= boardSize):
                 self.end()
                 sys.exit()
 
@@ -71,15 +76,23 @@ class Snake():
             self.eat()
 
         # oldSnake? oldSnake?!?! OLDSNAKE!!!!!!!!!!!!
+        # http://en.wikipedia.org/wiki/Metal_Gear_Solid
 
     def eat(self):
         self.coin.color = "#0666D0"
         self.snake += [self.coin]
 
-        self.coin = PhysicalObject(random.randint(4, 22) * 20, random.randint(4, 22) * 20, 10, "blue")
+        print boardSize/ (2 * snakeSize)
+        self.coin = PhysicalObject(random.randint(8, boardSize/ (2 * snakeSize) - (8 * snakeSize)) * 2 * snakeSize,
+                                   random.randint(8, boardSize/ (2 * snakeSize) - (8 * snakeSize)) * 2 * snakeSize, 
+                                   snakeSize, 
+                                   "blue")
 
         while any([self.snake[i].collides(self.coin) for i in xrange(len(self.snake))]): # any(x) returns x[0] or x[1] or x[2]....
-            self.coin.x, self.coin.y = random.randint(4, 22) * 20, random.randint(4, 22) * 20 # randomly reassign coin      
+            self.coin.x, self.coin.y = random.randint(8, boardSize/ (2 * snakeSize) - (8 * snakeSize)) * 2 * snakeSize,
+                                       random.randint(8, boardSize/ (2 * snakeSize) - (8 * snakeSize)) * 2 * snakeSize
+            
+            # randomly reassign coin if it has been placed inside the snake by accident     
 
     def end(self):
         highScore = 0
@@ -98,6 +111,6 @@ class Snake():
             segment.draw(self.canvas)
 
         self.coin.draw(self.canvas)
-        self.canvas.create_text(450, 50, text = str(len(self.snake)))
+        self.canvas.create_text(500, 50, text = str(len(self.snake)))
 
 Snake()
