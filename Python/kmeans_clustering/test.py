5cd44dd0ca82f258532c9d7743d182d53376edc4
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-05-02 03:23:06 -0400

    test now produces two clusters as a demonstration for use in demo

--- Python/kmeans_clustering/test.py
+++ Python/kmeans_clustering/test.py
@@ -1,11 +1,9 @@
-import random, helper
+import random
 from Tkinter import *
 root = Tk()
 canvas = Canvas(root, height = 500, width = 500)
 canvas.pack()
 
-canvas.create_text(150, 150, text = "p")
-canvas.create_text(350, 350, text = "p")
 
 x_plots = []
 
@@ -16,6 +14,12 @@ def cluster(x, y, points):
         color = helper.generateRandomColor()
         canvas.create_oval(plot_x - 5, plot_y - 5, plot_x + 5, plot_y + 5, fill = color, outline = color)
 
+cluster(150, 150, 500)
+canvas.create_oval(140, 140, 160, 160, fill = "white")
+canvas.create_text(150, 150, text = "c")
 
-cluster(250, 250, 1000)
+cluster(350, 350, 500)
+canvas.create_oval(340, 340, 360, 360, fill = "white")
+canvas.create_text(350, 350, text = "c")
+
 root.mainloop()
